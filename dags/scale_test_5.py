edcef512cd9cc2250f7b0af5b6939009e9709b34
Tony Huinker <ahuinker@gmail.com>
2022-10-21 19:52:54 -0400

    no dynamic task mapping

---
+++ dags/scale_test_5.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+def _sleep_pyop(secs):
+    from time import sleep
+    print(f"sleeping for {secs} seconds.")
+    sleep(secs)
+
+with DAG(
+        dag_id=f"scale_test_5",
+        schedule_interval=timedelta(seconds=1),
+        start_date=datetime(2022, 10, 20),
+        catchup=False,
+        max_active_runs=2005, 
+        max_active_tasks=2005
+    ) as dag:
+        sleep = PythonOperator(
+            task_id="sleep_1",
+            python_callable=_sleep_pyop,
+            op_kwargs= {
+                "secs": 3600
+                }
+            )
